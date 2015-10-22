54c9544668747b748138d9543ca5edd1ca927cf0
Jon Fredelius <jonfredelius@gmail.com>
2015-10-22 14:49:35 +0200

    Added some yml docs and more test stuff

---
+++ test/Survey/SurveyControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Idun\Survey;
+
+/**
+ * Test class for the Survey module
+ *
+ */
+class SurveyControllerTest extends \PHPUnit_Framework_TestCase
+{
+
+    $survey = new \Idun\Survey\SurveyController();
+    $di    = new \Anax\DI\CDIFactoryDefault();
+    $survey->setDI($di);
+    
+    // Inject the database service
+    $this->setShared('db', function() {
+        $db = new \Mos\Database\CDatabaseBasic();
+        $db->setOptions(require ANAX_APP_PATH . 'config/database_mysql.php');
+        $db->connect();
+        return $db;
+    });
+    
+}
