eff0ab272ce0b3cae77f8185592f181972e02206
Jon Fredelius <jonfredelius@gmail.com>
2015-10-22 20:26:16 +0200

    Trying to fix failing method definition in SurveyControllerTest

--- test/Survey/SurveyControllerTest.php
+++ test/Survey/SurveyControllerTest.php
@@ -9,17 +9,25 @@ namespace Idun\Survey;
 class SurveyControllerTest extends \PHPUnit_Framework_TestCase
 {   
     
-    $survey = new \Idun\Survey\SurveyController();
-    $di    = new \Anax\DI\CDIFactoryDefault();
-    $survey->setDI($di);
+    /*
+     * Useless test method
+     *
+     */
+    public function testNoneExistingMethod() {
     
-    // Inject the database service
-    $this->setShared('db', function() {
-        $db = new \Mos\Database\CDatabaseBasic();
-        $db->setOptions(require ANAX_APP_PATH . 'some_database_file');
-        $db->connect();
-        return $db;
-    });
+        $survey = new \Idun\Survey\SurveyController();
+        $di    = new \Anax\DI\CDIFactoryDefault();
+        $survey->setDI($di);
+    
+        // Inject the database service
+        $this->setShared('db', function() {
+            $db = new \Mos\Database\CDatabaseBasic();
+            $db->setOptions(require ANAX_APP_PATH . 'some_database_file');
+            $db->connect();
+            return $db;
+        });
+    
+    }
     
     
 }
